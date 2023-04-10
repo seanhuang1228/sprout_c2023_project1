ae646055a202fdf82bc0e61bb98d60c85e08c56b
Chi-Chun Chang <cjchang925@gmail.com>
2023-04-10 19:45:35 +0800

    fix: Remove redundant space.

--- release/ai.cpp
+++ release/ai.cpp
@@ -1,6 +1,6 @@
 #include "splendor.hpp"
 
-void ai (Pos* pos1, Pos* pos2) {
+void ai(Pos* pos1, Pos* pos2) {
   // TODO: prob 4.
   pos1->x = gen_rand() % BOARD_HEIGHT;
   pos1->y = gen_rand() % BOARD_WIDTH;
