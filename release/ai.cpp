86c8e4d6b22ec15c360f8f40f3bf105ad181ce18
Chi-Chun Chang <cjchang925@gmail.com>
2023-04-10 20:30:57 +0800

    feat: Add comments in release/ai.cpp to remind the students to remove the existing code.

--- release/ai.cpp
+++ release/ai.cpp
@@ -2,6 +2,7 @@
 
 void ai(Pos* pos1, Pos* pos2) {
   // TODO: prob 4.
+  // You should remove the code below and determine the four values by yourself.
   pos1->x = gen_rand() % BOARD_HEIGHT;
   pos1->y = gen_rand() % BOARD_WIDTH;
   pos2->x = gen_rand() % BOARD_HEIGHT;
